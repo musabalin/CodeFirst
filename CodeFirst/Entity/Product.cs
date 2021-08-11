d61c5bbd5fedf2650c4c339af35eae19f4f3b8af
MUSA BALIN <20164753049@ogr.akdeniz.edu.tr>
2021-08-11 23:25:15 +0300

    Add project files.

---
+++ CodeFirst/Entity/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Entity
+{
+    public class Product:DbContext
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductBrand { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductStock { get; set; }
+        public string ProductDetails { get; set; }
+    }
+}
